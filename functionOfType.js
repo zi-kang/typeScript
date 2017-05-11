637482a4e7504e3b1ca0e0695dbd3ce44118ad94
huzikang <huzikang@sightp.com>
2017-05-11 18:13:34 +0800

    hygsdvhjsdbvh

---
+++ functionOfType.js
@@ -0,0 +1,8 @@
+function add(x, y) {
+    return x + y;
+}
+var myAdd = function (x, y) {
+    return x + y;
+};
+var myAddFunc = function (x, y) { return x + y; };
+var myAddFuncc = function (x, y) { return x + y; };
