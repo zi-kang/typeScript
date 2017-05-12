2f6ec562694c8abedc9d486b8774a96bc98270ea
huzikang <huzikang@sightp.com>
2017-05-12 10:13:23 +0800

    dsgyvajsgvsdhm

--- functionOfType.js
+++ functionOfType.js
@@ -6,3 +6,13 @@ var myAdd = function (x, y) {
 };
 var myAddFunc = function (x, y) { return x + y; };
 var myAddFuncc = function (x, y) { return x + y; };
+//don't know the number of parameter
+function buildName(firstName) {
+    var restOfName = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        restOfName[_i - 1] = arguments[_i];
+    }
+    return firstName + " " + restOfName.join(" ");
+}
+var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+console.log(employeeName);
--- functionOfType.ts
+++ functionOfType.ts
@@ -7,3 +7,13 @@ let myAdd = function(x: number, y: number):number{
 let myAddFunc: (x: number, y: number) => number = function(x: number, y: number): number { return x+y; }
 let myAddFuncc: (baseValue: number, increment: number) => number =
     function(x,y){ return x + y; };
+
+//don't know the number of parameter
+function buildName(firstName: string, ...restOfName: string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+let employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+console.log( employeeName );
+
+//this && 箭头函数
