ba09ce956a3d63f75cdf5e7c17aef4fbe50f5a3e
huzikang <huzikang@sightp.com>
2017-05-15 17:39:09 +0800

    "新增this相对标准的用法"

--- functionOfType.js
+++ functionOfType.js
@@ -45,3 +45,18 @@ var deck = {
 var cardPicker = deck.createCardPicker();
 var pickedCard = cardPicker();
 console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+var deck1 = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        var _this = this;
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: _this.suits[pickedSuit], card: pickedCard % 13 };
+        };
+    }
+};
+var cardPicker1 = deck.createCardPicker();
+var pickedCard1 = cardPicker1();
+alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
--- functionOfType.ts
+++ functionOfType.ts
@@ -46,3 +46,31 @@ let deck = {
 let cardPicker = deck.createCardPicker();
 let pickedCard = cardPicker();
 console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+//正确使用this的形式
+interface Card{
+    suit: string;
+    card: number;
+}
+interface Deck{
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card;
+}
+let deck1: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function(this: Deck) {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker1 = deck.createCardPicker();
+let pickedCard1 = cardPicker1();
+
+alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
