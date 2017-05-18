e6e9442de8f2a1b1f2729aec50902cb7d8aa8049
huzikang <huzikang@sightp.com>
2017-05-18 17:04:52 +0800

    完成命名空间的学习（基础性）

---
+++ nameSpace/Test.js
@@ -0,0 +1,15 @@
+/// <reference path="Validation.ts" />
+/// <reference path="LettersOnlyValidator.ts" />
+/// <reference path="ZipCodeValidator.ts" />
+// Some samples to try
+var strings = ["Hello", "98052", "101"];
+// Validators to use
+var validators = {};
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
+// Show whether each string passed each validator
+strings.forEach(function (s) {
+    for (var name_1 in validators) {
+        console.log(" '' + s + '' " + (validators[name_1].isAcceptable(s) ? " matches " : " does not match ") + name_1);
+    }
+});
---
+++ nameSpace/Test.ts
@@ -0,0 +1,16 @@
+/// <reference path="Validation.ts" />
+/// <reference path="LettersOnlyValidator.ts" />
+/// <reference path="ZipCodeValidator.ts" />
+
+// Some samples to try
+let strings = ["Hello", "98052", "101"];
+// Validators to use
+let validators: { [s: string]: Validation.StringValidator; } = {};
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
+// Show whether each string passed each validator
+strings.forEach(s => {
+    for (let name in validators) {
+        console.log(" '' + s + '' " + (validators[name].isAcceptable(s) ? " matches " : " does not match ") + name);
+    }
+});
